e6eb3d3ad060659597d6e8d9b8e32a08c4c57257
bestboontech <bestboontech@gmail.com>
2022-04-21 00:17:36 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
- * reset_to_98 -updates the value * n points to .
- * @n: the number to modify.
+ *reset_to_98 -updates a integers value to 98
+ *@n: A pointer to an int that will be updated
+ *
+ *Return: void that means it is correct 
  */
-
-void  reset_to_98(int *n)
+void reset_to_98(int *n)
 {
-       *n=98;
+ *n = 98
 }
