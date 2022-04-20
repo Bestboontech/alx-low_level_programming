4927c1882c33e928739809c7ceace5dbb961fddb
bestboontech <bestboontech@gmail.com>
2022-04-20 23:46:22 +0100

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+in_putchar(char c);
+voiswap_int(int *a, int *b);
+in_strlen(char *s);
+voi_puts(char *str);
+voiprint_rev(char *s);
+voirev_string(char *s);
+voiputs2(char *str);
+voiputs_half(char *str);
+voiprint_array(int *a, int n);
+cha*_strcpy(char *dest, char *src);
+in_atoi(char *s);
