2da60d25f36e1847c0f33c33a6646050706087db
bestboontech <bestboontech@gmail.com>
2022-04-21 00:03:12 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
-#include "main.
+#include "main.h"
 
 /**
- * reset_to_98 - updates the value *n points to.
+ * reset_to_98 -updates the value * n points to .
  * @n: the number to modify.
  */
 
-void reset_to_98(int *n)
+void  reset_to_98(int *n)
 {
-	*n = 98;
+       *n=98;
 }
