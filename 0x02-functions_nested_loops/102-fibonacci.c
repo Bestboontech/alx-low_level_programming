4e5f3c5e30f13a86a7d72dc2282cdda085da27cb
bestboontech <bestboontech@gmail.com>
2022-04-26 04:55:28 +0100

    103-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,19 +6,15 @@
  */
 int main(void)
 {
-	long int i, x = 1, y = 2, sum = 0, tSum = 0;
-
-	for (i = 0; i < 49; i++)
-	{
-		if ((y % 2 == 0) && (y <= 4000000))
-		{
-			tSum = tSum + y;
-		}
-		sum = x + y;
-		x = y;
-		y = sum;
-
-	}
-	printf("%ld\n", tSum);
-	return (0);
+long int i, x = 1, y = 2, sum = 0;
+for (i = 0; i < 49; i++)
+{
+printf("%ld, ", x);
+sum = x + y;
+x = y;
+y = sum;
+if (i == 48)
+printf("%ld\n", x);
+}
+return (0);
 }
