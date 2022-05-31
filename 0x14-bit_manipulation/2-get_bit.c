6daeb794eb4ac460084e5ccd0e46b7df4739b8e9
bestboontech <bestboontech@gmail.com>
2022-05-31 05:04:14 +0100

    main.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,15 +3,19 @@
 /**
  * get_bit - A function that gets a bit at index
  * @n: The number to get bit from
+ * @index: The index where the bit get at
  * Return: The value of the bit or -1 if an error occured
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index > (sizeof(index) * 8))
-return (-1);
-if ((n >> index) & 1)
-return (1);
-return (0);
-}
+	unsigned long int max = 0x01;
 
+	max <<= index;
+	if (max == 0)
+		return (-1);
+
+	if ((n & max))
+		return (1);
+	else
+		return (0);
+}
